134f4ddb53287fa39dc6d58cebba8c1c1ebd880e
Nadhaniel <45491434+Nadhaniel@users.noreply.github.com>
2022-11-16 14:55:49 +0000

    Setup

---
+++ MASE/Assets/Scripts/Data/TerrainData.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu()]
+public class TerrainData : UpdateableData
+{
+    public float meshHeightMultplier;
+    public AnimationCurve meshHeightCurve;
+}
