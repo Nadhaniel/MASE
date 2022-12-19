cdb24ebdf8027afa7c1c04affa3169f4eda238b5
Nadhaniel <45491434+Nadhaniel@users.noreply.github.com>
2022-12-19 14:41:26 +0000

    Generating Capsule mesh method

--- MASE - Agent Creator/Assets/Scripts/DetailedCapsule.cs
+++ MASE - Agent Creator/Assets/Scripts/DetailedCapsule.cs
@@ -13,6 +13,11 @@ public class DetailedCapsule : MonoBehaviour
     public int rings = 0;
     public float depth = 1.0f;
     public float radius = 0.5f;
+    public float height = 2f;
+    public int segments = 24;
+
+    int points;
+    Vector3[] vertices;
     Mesh CapsuleMesh;
 
     static GameObject InstantMesh(string name, Mesh mesh, float capsuleDepth = 1.0f, float capsuleRadius = 0.5f) { //instatiates basic capsule parameters and creates capsule object
@@ -29,6 +34,73 @@ public class DetailedCapsule : MonoBehaviour
 
         return capsule;
     }
+
+    void GenerateCapsuleMesh() {
+        if (segments % 2 != 0)
+        { 
+            segments++;
+        }
+
+        points = segments + 1;
+
+        float[] pX = new float[points];
+        float[] pZ = new float[points];
+        float[] pY = new float[points];
+        float[] pR = new float[points];
+
+        float calcH = 0f;
+        float calcV = 0f;
+
+        for (int i = 0; i < points; i++)
+        {
+            pX[i] = Mathf.Sin(calcH * Mathf.Deg2Rad);
+            pZ[i] = Mathf.Cos(calcH * Mathf.Deg2Rad);
+            pY[i] = Mathf.Cos(calcH * Mathf.Deg2Rad);
+            pR[i] = Mathf.Cos(calcH * Mathf.Deg2Rad);
+
+            calcH += 360f / (float)segments;
+            calcV += 180f / (float)segments;
+        }
+
+        // -Vertices and UVs
+
+        vertices = new Vector3[points * (points + 1)];
+        Vector2[] uvs = new Vector2[vertices.Length];
+
+        //Y 0ffset is half the heigh minus the diameter
+        float yOff = (height - (radius * 2f)) * 0.5f;
+
+        if (yOff < 0) {
+            yOff = 0;
+        }
+
+        //uv calculations
+        float stepX = 1f / ((float)(points - 1));
+        float uvX, uvY;
+
+        //Top Hemisphere
+        int top = Mawthf.CeilToInt((float)points * 0.5f);
+
+        int ind = 0;
+        for (int y = 0; y < top; y++)
+        {
+            for (int x = 0; x < top; x++)
+            {
+                vertices[ind] = new Vector3(pX[x] * pR[y], pY[y], pZ[x] * pR[y]) * radius;
+                vertices[ind].y = yOff + vertices[ind].y;
+
+                uvX = 1f - (stepX * (float)x);
+                uvY = (vertices[ind].y + (height * 0.5f)) / height;
+                uvs[ind] = new Vector2(uvX, uvY);
+
+                ind++;
+            }
+        }
+
+        //Bottom Hemisphere
+        int btm = Mathf.FloorToInt
+    }
+
 
     Mesh CapsuleData()
     {
