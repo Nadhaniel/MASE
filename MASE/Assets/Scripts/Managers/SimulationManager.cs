60357904af2c3af9c087d256c70009e93ff51b43
Nadhaniel <45491434+Nadhaniel@users.noreply.github.com>
2023-03-06 19:57:25 +0000

    Made new way to spawn creatures

--- MASE/Assets/Scripts/Managers/SimulationManager.cs
+++ MASE/Assets/Scripts/Managers/SimulationManager.cs
@@ -4,24 +4,24 @@ using UnityEngine;
 
 public class SimulationManager : MonoBehaviour
 {
-    public int MaxPopulation = 150;
+    private int MaxPopulation;
     public GameObject creature;
-    public GameObject FittestAgent;
     List<GameObject> AgentPopulation = new List<GameObject>();
     List<GameObject> sortedList = new List<GameObject>();
     public static float elapsedtime = 0;
     public float timeScale = 1;
     int generation = 0;
     public float trialTime = 10;
-    public GameObject spawner;
+
+    //New Generation system
+
+    private float minHeight;
+    private float maxHeight;
 
     private void Start()
     {
-        for (int i = 0; i < MaxPopulation; i++)
-        {
-            GameObject agent = creature;
-            AgentPopulation.Add(Instantiate(agent, spawner.transform.position, spawner.transform.rotation));
-        }
+        MaxPopulation = 400;
+        Generate();
         generation += 1;
         Time.timeScale = timeScale;
     }
@@ -29,109 +29,141 @@ public class SimulationManager : MonoBehaviour
     private void Update()
     {
         elapsedtime += Time.deltaTime;
-        //Debug.Log(elapsedtime);
-        //sortedList = AgentPopulation.OrderByDescending(x => x.GetComponent<Creature>().Fitness).ToList();
         if (elapsedtime >= trialTime)
         {
-            //FittestAgent = sortedList.First();
-            //Debug.Log("Fittest Creature: " + FittestAgent.GetComponent<Creature>().Fitness);
-            BreedPopNew();
+            Generate();
             elapsedtime = 0;
         }
     }
 
-    private GameObject Breed(GameObject parent1, GameObject parent2) //May move this method to creature class as its more appropriate there
-    {
-        GameObject offspring = Instantiate(creature, spawner.transform.position, spawner.transform.rotation);
-        DNA newDNA = new DNA();
-        offspring.GetComponent<Creature>().dna = newDNA;
-        offspring.GetComponent<Creature>().brain = parent1.GetComponent<Creature>().brain;
-        if (Random.Range(0, 3) == 1)
-        {
-            offspring.GetComponent<Creature>().brain.MutateBrain();
-            offspring.GetComponent<Creature>().dna.Combine(parent1.GetComponent<Creature>().dna.Genes, parent2.GetComponent<Creature>().dna.Genes);
-            offspring.GetComponent<Creature>().dna.RandomizeGeneSet(parent1.GetComponent<Creature>().dna.getGene("Mutation_Size")); //Mutates a number of genes dependant on mutation_size
-        }
-        else
-        {
-            offspring.GetComponent<Creature>().isChild = true;
-            offspring.GetComponent<Creature>().dna.Combine(parent1.GetComponent<Creature>().dna.Genes, parent2.GetComponent<Creature>().dna.Genes);
-        }
-        return offspring;
-    }
+    //private GameObject Breed(GameObject parent1, GameObject parent2) //May move this method to creature class as its more appropriate there
+    //{
+    //    GameObject offspring = Instantiate(creature, spawner.transform.position, spawner.transform.rotation);
+    //    DNA newDNA = new DNA();
+    //    offspring.GetComponent<Creature>().dna = newDNA;
+    //    offspring.GetComponent<Creature>().brain = parent1.GetComponent<Creature>().brain;
+    //    if (Random.Range(0, 3) == 1)
+    //    {
+    //        offspring.GetComponent<Creature>().brain.MutateBrain();
+    //        offspring.GetComponent<Creature>().dna.Combine(parent1.GetComponent<Creature>().dna.Genes, parent2.GetComponent<Creature>().dna.Genes);
+    //        offspring.GetComponent<Creature>().dna.RandomizeGeneSet(parent1.GetComponent<Creature>().dna.getGene("Mutation_Size")); //Mutates a number of genes dependant on mutation_size
+    //    }
+    //    else
+    //    {
+    //        offspring.GetComponent<Creature>().isChild = true;
+    //        offspring.GetComponent<Creature>().dna.Combine(parent1.GetComponent<Creature>().dna.Genes, parent2.GetComponent<Creature>().dna.Genes);
+    //    }
+    //    return offspring;
+    //}
 
-    private void BreedPopNew()
-    {
-        sortedList = AgentPopulation.OrderByDescending(o => o.GetComponent<Creature>().Fitness).ToList();
+    //private void BreedPopNew()
+    //{
+    //    sortedList = AgentPopulation.OrderByDescending(o => o.GetComponent<Creature>().Fitness).ToList();
 
-        string Generation = "Generation: " + this.generation;
-        foreach (GameObject g in sortedList)
-        {
-            Generation += ", " + g.GetComponent<Creature>().Fitness;
-        }
-        //Debug.Log("Fitness: " + Generation);
-        AgentPopulation.Clear();
+    //    string Generation = "Generation: " + this.generation;
+    //    foreach (GameObject g in sortedList)
+    //    {
+    //        Generation += ", " + g.GetComponent<Creature>().Fitness;
+    //    }
+    //    //Debug.Log("Fitness: " + Generation);
+    //    AgentPopulation.Clear();
 
-        while (AgentPopulation.Count < MaxPopulation)
-        {
-            int bestParentCutoff = sortedList.Count / 4;
-            for (int i = 0; i < bestParentCutoff - 1; i++)
-            {
-                for (int j = 1; j < bestParentCutoff; j++)
-                {
-                    AgentPopulation.Add(Breed(sortedList[i], sortedList[j]));
-                    if (AgentPopulation.Count == MaxPopulation) break;
-                    AgentPopulation.Add(Breed(sortedList[j], sortedList[i]));
-                    if (AgentPopulation.Count == MaxPopulation) break;
-                }
-                if (AgentPopulation.Count == MaxPopulation) break;
-            }
-        }
-        for (int i = 0; i < sortedList.Count; i++)
-        {
-            Destroy(sortedList[i]);
-        }
-        this.generation++;
-    }
+    //    while (AgentPopulation.Count < MaxPopulation)
+    //    {
+    //        int bestParentCutoff = sortedList.Count / 4;
+    //        for (int i = 0; i < bestParentCutoff - 1; i++)
+    //        {
+    //            for (int j = 1; j < bestParentCutoff; j++)
+    //            {
+    //                AgentPopulation.Add(Breed(sortedList[i], sortedList[j]));
+    //                if (AgentPopulation.Count == MaxPopulation) break;
+    //                AgentPopulation.Add(Breed(sortedList[j], sortedList[i]));
+    //                if (AgentPopulation.Count == MaxPopulation) break;
+    //            }
+    //            if (AgentPopulation.Count == MaxPopulation) break;
+    //        }
+    //    }
+    //    for (int i = 0; i < sortedList.Count; i++)
+    //    {
+    //        Destroy(sortedList[i]);
+    //    }
+    //    this.generation++;
+    //}
     
-    private void BreedPop()
-    {
-        if (generation == 1)
-        {
-            ClearDead(AgentPopulation);
-            AgentPopulation.Clear();
-            GameObject child = FittestAgent;
-            AgentPopulation.Add(Instantiate(child, spawner.transform.position, spawner.transform.rotation));
-            for (int i = 0; i < MaxPopulation; i++)
-            {
-                GameObject agent = Instantiate(creature, spawner.transform.position, spawner.transform.rotation);
-                AgentPopulation.Add(agent);
-            }
-            generation += 1;
-        }
-        else
-        {
-            FittestAgent = Breed(FittestAgent, sortedList.First());
-            ClearDead(AgentPopulation);
-            AgentPopulation.Clear();
-            for (int i = 0; i < MaxPopulation; i++)
-            {
-                GameObject agent = Instantiate(creature, spawner.transform.position, spawner.transform.rotation);
-                AgentPopulation.Add(agent);
-            }
-            generation += 1;
-        }
+    //private void BreedPop()
+    //{
+    //    if (generation == 1)
+    //    {
+    //        ClearDead(AgentPopulation);
+    //        AgentPopulation.Clear();
+    //        GameObject child = FittestAgent;
+    //        AgentPopulation.Add(Instantiate(child, spawner.transform.position, spawner.transform.rotation));
+    //        for (int i = 0; i < MaxPopulation; i++)
+    //        {
+    //            GameObject agent = Instantiate(creature, spawner.transform.position, spawner.transform.rotation);
+    //            AgentPopulation.Add(agent);
+    //        }
+    //        generation += 1;
+    //    }
+    //    else
+    //    {
+    //        FittestAgent = Breed(FittestAgent, sortedList.First());
+    //        ClearDead(AgentPopulation);
+    //        AgentPopulation.Clear();
+    //        for (int i = 0; i < MaxPopulation; i++)
+    //        {
+    //            GameObject agent = Instantiate(creature, spawner.transform.position, spawner.transform.rotation);
+    //            AgentPopulation.Add(agent);
+    //        }
+    //        generation += 1;
+    //    }
             
+    //}
+
+    //private void ClearDead(List<GameObject> Population)
+    //{
+    //    foreach (GameObject creature in Population)
+    //    {
+    //        if (creature.GetComponent<Creature>().isdead == true)
+    //        {
+    //            Destroy(creature);
+    //        }
+    //    }
+    //}
+
+    public void Generate()
+    {
+        Clear();
+        Vector2 xRange = new Vector2(-1000, 1000);
+        Vector2 zRange = new Vector2(-1000, 1000);
+        minHeight = 16;
+        maxHeight = 20;
+        int i = 0;
+        while (i < MaxPopulation)
+        {
+            float sampleX = Random.Range(xRange.x, xRange.y);
+            float sampleY = Random.Range(zRange.x, zRange.y);
+            Vector3 rayStart = new Vector3(sampleX, maxHeight, sampleY);
+            if (Physics.Raycast(rayStart, Vector3.down, out RaycastHit hit, Mathf.Infinity))
+            {
+                if (hit.point.y > minHeight)
+                {
+                    AgentPopulation.Add(Instantiate(creature, hit.point, Quaternion.identity));
+                    //instatiatedPrefab.transform.position = hit.point;
+                    i++;
+                }
+            }
+        }
     }
 
-    private void ClearDead(List<GameObject> Population)
+    public void Clear()
     {
-        foreach (GameObject creature in Population)
+        for (int i = 0; i < AgentPopulation.Count; i++)
         {
-            if (creature.GetComponent<Creature>().isdead == true)
-            {
-                Destroy(creature);
-            }
+            DestroyImmediate(AgentPopulation[i]);
         }
-    }                                                                 
+    }
+
+
+
 }
